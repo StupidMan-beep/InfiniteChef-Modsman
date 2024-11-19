392b1639911b3763dbd44b44f43fdc9530974dfc
StupidMan-beep <giancarlo.venturi@outlook.com>
2024-11-19 10:28:28 +0800

    Create human.js

---
+++ human.js
@@ -0,0 +1,9 @@
+addIngredient("human",{
+    color:"000000"
+    innerColor:"ffeda4"
+    type:"meat"
+    reactions: {
+        water: { set1:"broth", tempMin:90 }
+    }
+
+addRecipe("human+broth", "human melt"
